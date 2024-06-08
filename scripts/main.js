17464e898096298b954e77420adb1c5a4a0a9262
Андрей Мокеев <amok2005@gmail.com>
2024-06-09 02:48:40 +0300

    v 09.06.1

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-        const car = document.getElementById('auto');
-        const wheel1 = document.getElementById('wheel1');
-        const wheel2 = document.getElementById('wheel2');
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    const car = document.getElementById("auto");
+    const wheel1 = document.getElementById("wheel1");
+    const wheel2 = document.getElementById("wheel2");
 
-        if (car) {
-            car.classList.add('car-moving');
-        }
+    if (car) {
+      car.classList.add("car-moving");
+    }
 
-        if (wheel1) {
-            wheel1.classList.add('wheels-rolling');
-        }
+    if (wheel1) {
+      wheel1.classList.add("wheels-rolling");
+    }
 
-        if (wheel2) {
-            wheel2.classList.add('wheels-rolling');
-        }
-    }, 2000);
+    if (wheel2) {
+      wheel2.classList.add("wheels-rolling");
+    }
+  }, 2000);
 });
